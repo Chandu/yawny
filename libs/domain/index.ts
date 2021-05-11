60e13174835bdcf5920a949f881dda41dbb8954d
SirishaAnupindi <sanupindi@thedmsgrp.com>
2021-05-11 08:51:49 +0530

    Adds another module to the domain

--- libs/domain/index.ts
+++ libs/domain/index.ts
@@ -1,5 +1,5 @@
 import helloDolly from 'hello-dolly';
 import {  v4 as uuidv4  } from 'uuid';
 export default {
-    name: `${helloDolly()} - ${uuidv4()} `
+    name: `${helloDolly()} - ${uuidv4()} World`
 }
---
+++ libs/domain/namer.ts
@@ -0,0 +1,7 @@
+import helloDolly from "hello-dolly";
+import {  v4 as uuidv4  } from 'uuid';
+
+const  boloDolly = {
+    name: `Bolo ${helloDolly()} - ${uuidv4()} World`
+}
+export default boloDolly;
